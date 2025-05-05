8ef832ba491d403f3c692a04d2c8bc2c9997c638
sukant43 <sukant98657@gmail.com>
2025-05-05 20:39:46 +0530

    sql+frontend finished

--- mern+sql/backend/server.js
+++ mern+sql/backend/server.js
@@ -2,6 +2,8 @@ const express=require('express')
 const app=express()
 const {pool}=require('./config/db')
 const route=require('./route/todoRoute')
+const cors=require('cors')
+app.use(cors())
 app.use(express.json())
 
 app.use('/api',route)
