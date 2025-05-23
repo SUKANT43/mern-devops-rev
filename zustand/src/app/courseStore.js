63cfc2f4acb4e7abbf58b3c3edada7c414e513fc
sukant43 <sukant98657@gmail.com>
2025-05-23 21:40:26 +0530

    store completed

--- zustand/src/app/courseStore.js
+++ zustand/src/app/courseStore.js
@@ -1,1 +1,22 @@
 import create from 'zustand';
+import {devtools, persist} from 'zustand/middleware';
+//persist is used to store the data in local storag
+
+const courseStore = (set) => ({
+  courses: [], // initial state
+
+  addCourse: (course) => {
+    set((state) => ({
+      courses: [...state.courses, course],
+    }));
+  },
+
+  removeCourse: (id) => {
+    set((state) => ({
+      courses: state.courses.filter((c) => c.id !== id),
+    }));
+  },
+});
+
+
+//state is used to store old value in the array
